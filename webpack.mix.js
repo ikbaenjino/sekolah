80f7345c4a641bc67ec44b757be299c9dc52d6f0
ikbaenjino <enjino@politeknikpgribanten.ac.id>
2025-06-09 11:58:32 +0700

    Update webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,6 @@
-.postCss('resources/css/app.css', 'public/css', [
-    require('postcss-import'),
-    require('tailwindcss'),
-    require('autoprefixer'),
-])
+mix.js('resources/js/app.js', 'public/js')
+   .postCss('resources/css/app.css', 'public/css', [
+     require('postcss-import'),
+     require('tailwindcss'),
+     require('autoprefixer'),
+   ]);
