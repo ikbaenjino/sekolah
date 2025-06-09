15ccfdcea19abfdf7df3bc4a25c20652688a1ec0
ikbaenjino <enjino@politeknikpgribanten.ac.id>
2025-06-09 12:40:25 +0700

    Update webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,15 @@
-mix.js('resources/js/app.js', 'public/js')
-   .postCss('resources/css/app.css', 'public/css', [
-     require('postcss-import'),
-     require('tailwindcss'),
-     require('autoprefixer'),
-   ]);
+// Versi minimal jika tidak menggunakan CSS
+const mix = require('laravel-mix');
+mix.js('resources/js/app.js', 'public/js');
+
+// Jika menggunakan CSS/Sass
+// .sass('resources/sass/app.scss', 'public/css')
+// atau
+// .postCss('resources/css/app.css', 'public/css')
+# Jika resources/js/app.js tidak ada
+mkdir -p resources/js && touch resources/js/app.js
+
+# Jika ingin menggunakan Sass
+mkdir -p resources/sass && touch resources/sass/app.scss
+npm install
+composer install
